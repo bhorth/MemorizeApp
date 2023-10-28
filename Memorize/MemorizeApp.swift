2649b403bcee633aae49ad42b77ba9573bbdfbd2
Brayden Horth <b.horth@hotmail.ca>
2023-10-28 11:53:46 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Brayden Horth on 2023-10-28.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
